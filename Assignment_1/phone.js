a4f49a06f6267685e4e2f8443d0f42edbf827034
JavierJRRamirez <Javier.Ramirez@colorado.edu>
2019-01-25 00:14:10 -0700

    Edit

--- Assignment_1/phone.js
+++ Assignment_1/phone.js
@@ -1,13 +1,28 @@
 //alert("hey")
 
 $(document).ready(function() { // do this when the document is loaded
-                  $("MainButton").show(); // show the element with ID "element"
-                  $("content").hide(); // hide the element with ID "otherElement"
+                  $("#MainButtonA").show(); // show the element with ID "element"
+                  $("#MainButtonB").show(); // show the element with ID "element"
+                  $("#MainButtonC").show(); // show the element with ID "element"
+                  $("#content2").hide(); // hide the element with ID "otherElement"
+                  $("#content3").hide(); // hide the element with ID "otherElement"
+
 });
 
-$("#content").click(function() { // when "button_id" is clicked
-                      $("#Dial").show(); // show element
-//                      $("#").hide();    // hide other element
+$("#MainButtonA").click(function() { // when "button_id" is clicked
+                      $("#content1").show(); // show element
+                      $("#content2").hide();    // hide other element
+                      $("#content3").hide(); // hide the element with ID "otherElement"
                       });
+$("#MainButtonB").click(function() { // when "button_id" is clicked
+                        $("#content2").show(); // show element
+                        $("#content1").hide();    // hide other element
+                        $("#content3").hide(); // hide the element with ID "otherElement"
+                        });
+$("#MainButtonC").click(function() { // when "button_id" is clicked
+                        $("#content3").show(); // show element
+                        $("#content1").hide();    // hide other element
+                        $("#content2").hide(); // hide the element with ID "otherElement"
+                        });
 
 
